b33cb644787b4455b193fce8e2734b992adb40b8
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-26 22:41:26 +0300

    Initial commit

---
+++ src/main/java/com/demo/springsecuritygenuinecoder/SpringSecurityGenuineCoderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springsecuritygenuinecoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityGenuineCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityGenuineCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springsecuritygenuinecoder/SpringSecurityGenuineCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springsecuritygenuinecoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityGenuineCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
